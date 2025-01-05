1426ef534451f30c3fd04c961b24f39d92151de7
BeeHexed <livingfree303@gmail.com>
2025-01-05 13:01:34 +0000

    troubleshooting

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 function handleSearchSubmit(event) {
     event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    console.log(searchInput.value);
+
 }
 
 let searchFormElement = document.querySelector("#search-form");
