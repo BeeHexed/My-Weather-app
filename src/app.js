53b7431cfac85b3bde57fb3e7fa85eb197d278cb
BeeHexed <livingfree303@gmail.com>
2025-01-06 22:16:11 +0000

    encoded URI

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ function refreshWeather(response) {
 
 function searchCity(city){
   let apiKey = "36b4fe238810fo977tebf4ca2bcf46b6";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodeURIComponent(city.trim())}&key=${apiKey}&units=metric`
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -24,3 +24,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("London");
